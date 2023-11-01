0ca27255d0f0cf8400c9215fdce067d3a2034fc3
Michael <vbifrhenyfdctulf@mail.ru>
2023-11-01 10:23:05 +0300

    Bette outputs.

--- mainn.cpp
+++ mainn.cpp
@@ -7,7 +7,7 @@
 int main(int argc, char* argv[]) {
     std::fstream inFile(argv[1]); // Open the input file
     if (!inFile.is_open()) {
-        std::cerr << "Unable to open file " << argv[1];
+        std::cerr << "Unable to open input file " << argv[1];
         return 1;   // Return with error
     }
     std::vector<int> numbers;
@@ -22,7 +22,7 @@ int main(int argc, char* argv[]) {
 
     std::ofstream outFile(argv[2]); // Open the output file
     if (!outFile.is_open()) {
-        std::cerr << "Unable to open file " << argv[2];
+        std::cerr << "Unable to open output file " << argv[2];
         return 1;   // Return with error
     }
 
